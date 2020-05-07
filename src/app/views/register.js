a771d2e50625e70caa6ccc838a05a54d0c0b0d0d
Rafael <rarafa245@gmail.com>
2020-05-07 07:31:12 -0300

    'Merging2'

--- src/app/views/register.js
+++ src/app/views/register.js
@@ -1,14 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD
 import NavBar from '../components/logginComponents/navbar'
 import RegisterForm from '../components/logginComponents/registerForm'
 import IntroduceBar from '../components/logginComponents/introduceBar'
 
-=======
-import NavBar from './../components/navbar'
-import RegisterForm from './../components/registerForm'
-import InfoCard from './../components/infoCard'
->>>>>>> origin
 
 class Register extends React.Component{
     // Register new client Page
@@ -19,7 +13,6 @@ class Register extends React.Component{
                 <header>
                     <NavBar />
                 </header>
-<<<<<<< HEAD
                 <div className="container-fluid">
                     <div className="row justify-content-lg-around">
                         <RegisterForm />
@@ -29,17 +22,6 @@ class Register extends React.Component{
                                     message='You can ask your questions to get answers from other users and store them for future reference'/>
                     </div>
                 </div>
-=======
-                <section className="container-fluid">
-                    <div className="row justify-content-lg-around">  
-                        <RegisterForm />
-                        <InfoCard  mode={'register'}
-                                            title={'Come on, Join This Community!'}
-                                            subtitle={'Free for all'}
-                                            message={'You can ask your questions to get answers from other users and store them for future reference'}/>
-                    </div>
-                </section>
->>>>>>> origin
             </div>
         )
     }
