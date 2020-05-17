9b68465d8c3b160d370676bde8b9caa105278b46
Rafael <rarafa245@gmail.com>
2020-05-17 09:56:54 -0300

    Refactoring logginForm Hook

--- src/app/components/logginComponents/logginForm.js
+++ src/app/components/logginComponents/logginForm.js
@@ -1,35 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 
-class LogginForm extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            email: '',
-            pass: '',
-            message: ''
-        }
-
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-
-    handleChange = (event) => {
-        // Change Input Callback - Change the value of the form inputs
-
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+function LogginForm (props){
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [message, setMessage] = useState()
     
 
-    handleSubmit = (event) => {
-        /* Submit Callback - Send values to Server
-            * Check if there´s a last page to return (Token expiration)
-            * Save infos in localStorage
-            * Redirect Client 
-        */
+    const handleSubmit = (event) => {
 
         event.preventDefault()
         const logginData = new FormData(event.target)
@@ -41,69 +20,63 @@ class LogginForm extends React.Component{
         .then(res => res.json())
         .then(res => {
             if (res.status){
+                setMessage()
                 localStorage.setItem('AWT', res.token)
                 localStorage.setItem('UID', res.username)
-                this.props.history.push({
+                props.history.push({
                     pathname: (localStorage.getItem('AWTST') == 'true' ?
                                 localStorage.getItem('LPC') : '/home')
                 })
-            }else{
-                this.setState({
-                    message: <Message type={'alert alert-danger text-center'} 
+            } else {
+                setMessage( <Message type={'alert alert-danger text-center'} 
                                 message='Email ou Senha Invalidos !' />
-                })
+                )
             }
         })
     }
 
 
-    render(){
-        return(
-            <div className="px-4 col-lg-6">
+    return (
+        <div className="px-4 col-lg-6">
 
-                <form onSubmit={this.handleSubmit} className="mt-4">
-                    <fieldset className="form-group">
-                        <legend className="border-bottom mb-4">Log In</legend>
+            <form onSubmit={ handleSubmit} className="mt-4">
+                <fieldset className="form-group">
+                    <legend className="border-bottom mb-4">Log In</legend>
 
-                        {this.state.message}
+                        {message}
 
-                        <div className="form-group">
-                            <label htmlFor="Email">Email address</label>
-                            <input id="Email"
-                                    name="email"
-                                    value={this.state.email}
-                                    onChange={this.handleChange}
-                                    type="email" 
-                                    className="form-control mb-2" 
-                                    aria-describedby="emailHelp" />
-                            
-                            <label htmlFor="Pass">Password</label>
-                            <input id="Pass"
-                                    name="pass"
-                                    onChange={this.handleChange}
-                                    value={this.state.pass} 
-                                    type="password" 
-                                    className="form-control" 
-                                    aria-describedby="emailHelp" />
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="Email">Email address</label>
+                        <input id="Email"
+                                name="email"
+                                value={email}
+                                onChange={ e => setEmail(e.target.value) }
+                                type="email" 
+                                className="form-control mb-2" 
+                                aria-describedby="emailHelp" />
+                        
+                        <label htmlFor="Pass">Password</label>
+                        <input id="Pass"
+                                name="pass"
+                                value={pass} 
+                                onChange= { e => setPass(e.target.value) }
+                                type="password" 
+                                className="form-control" 
+                                aria-describedby="emailHelp" />
+                    </div>
 
-                        <button type="submit" 
-                            className="btn bg-steel text-white">
-                            Log In
-                        </button>
-                    </fieldset>
-                </form>
-                
-            </div>
-        )
-    }
+                    <button type="submit" 
+                        className="btn bg-steel text-white">
+                        Log In
+                    </button>
+                </fieldset>
+            </form>
+            
+        </div>
+    )
 }
 
 function Message(props) {
-    /* Success or Falue alert
-        :parram - type: Type of the message
-                message: Info message
-    */
 
     return(
         <div className={props.type} role="alert">
