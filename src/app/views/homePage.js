c7404102d20c0e25ff450c1f6e87a3985d78238e
Rafael <rarafa245@gmail.com>
2020-05-17 14:24:15 -0300

    Refactoring Home Hooks

--- src/app/views/homePage.js
+++ src/app/views/homePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { loadPage } from '../config'
 import LeftSideBar from '../components/homeComponents/leftSideBar'
 import UserCard from '../components/homeComponents/userCard'
@@ -7,55 +7,47 @@ import ManageContactsSide from '../components/homeComponents/manageContactsSide'
 import QuestionGroup from '../components/homeComponents/questionGroup'
 
 
-class Home extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            userImage: "",
-            questions: "" 
-        }
-    }
+function Home (props) {
     
-    componentDidMount(){
-        localStorage.setItem('LPC', this.props.location.pathname)
+    useEffect(() => {
+        localStorage.setItem('LPC', props.location.pathname)
         localStorage.setItem('AWTST', 'true')
         loadPage()
-    }
+    })
 
-    render(){
-        return(
-            <div>
-                <header>
-                    <HomeNav />
-                </header>
+    return (
+        <div>
+            <header>
+                <HomeNav />
+            </header>
 
-                <section className="container-fluid">
-                    <div className="row">
+            <section className="container-fluid">
+                <div className="row">
 
-                        <aside className="col-3 mt-2 px-5
-                                            d-none d-sm-none d-md-block
-                                            ">
-                            <LeftSideBar />
-                        </aside>
+                    <aside className="col-3 mt-2 px-5
+                                        d-none d-sm-none d-md-block
+                                        ">
+                        <LeftSideBar />
+                    </aside>
 
-                        <section className="col-6 mt-2 px-md-3 px-lg-0
-                                            col-12 col-md-9 col-lg-6
-                                            ">
-                            <UserCard mode="home"/>
-                            <QuestionGroup />
-                        </section>
+                    <section className="col-6 mt-2 px-md-3 px-lg-0
+                                        col-12 col-md-9 col-lg-6
+                                        ">
+                        <UserCard mode="home"/>
+                        <QuestionGroup />
+                    </section>
 
-                        <aside className="col-3 mt-2 px-5
-                                            d-none d-sm-none d-md-none d-lg-block  
-                                            ">
-                            <ManageContactsSide />
-                        </aside>
+                    <aside className="col-3 mt-2 px-5
+                                        d-none d-sm-none d-md-none d-lg-block  
+                                        ">
+                        <ManageContactsSide />
+                    </aside>
 
-                    </div>
-                </section>
-            </div>
-        )
-    }
+                </div>
+            </section>
+        </div>
+    )
+    
 }
 
 export default Home
