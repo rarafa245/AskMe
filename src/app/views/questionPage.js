2b1271e94b41ec808d8c5fb4c794aec796c0ec27
Rafael <rarafa245@gmail.com>
2020-05-17 14:34:08 -0300

    Refactoring QuestionPage Hooks

--- src/app/views/questionPage.js
+++ src/app/views/questionPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { loadPage } from '../config'
 import HomeNav from '../components/homeComponents/homeNav'
 import UserCard from '../components/homeComponents/userCard'
@@ -6,39 +6,38 @@ import QuestionsForm from '../components/questionsComponents/questionsForm'
 import QuestionGroup from '../components/homeComponents/questionGroup'
 
 
-class QuestionPage extends React.Component{
+function QuestionPage (props) {
 
-    componentDidMount(){
-        localStorage.setItem('LPC', this.props.location.pathname)
+    useEffect(() => {
+        localStorage.setItem('LPC', props.location.pathname)
         localStorage.setItem('AWTST', 'true')
         loadPage()
-    }
-    
-    render(){
-        return(
-            <div>
-                <header>
-                    <HomeNav />
-                </header>
-                <section className="container-fluid">
-                    <div className="row justify-content-around">
-                        <div className="mt-2
-                                        col-md-5">
-                            <UserCard mode='question'/>
-                            <div className="d-none d-sm-block">
-                                <QuestionGroup />
-                            </div>
-                        </div>
-                        <div className="row mt-2
-                                            col-md-6">
-                            <QuestionsForm />
+    }, [])
+
+    return(
+        <div>
+            <header>
+                <HomeNav />
+            </header>
+            <section className="container-fluid">
+                <div className="row justify-content-around">
+                    <div className="mt-2
+                                    col-md-5">
+                        <UserCard mode='question'/>
+                        <div className="d-none d-sm-block">
+                            <QuestionGroup />
                         </div>
                     </div>
-                    
-                </section>
-            </div>
-        )
-    }
+                    <div className="row mt-2
+                                        col-md-6">
+                        <QuestionsForm />
+                    </div>
+                </div>
+                
+            </section>
+        </div>
+    )
+    
 }
 
 export default QuestionPage
