3616ecef2d504e56a0bbb168d44bd6d02bc7d303
Rafael <rarafa245@gmail.com>
2020-05-17 13:47:01 -0300

    Refactoring homeNav / userCard Hooks

--- src/app/components/homeComponents/homeNav.js
+++ src/app/components/homeComponents/homeNav.js
@@ -1,84 +1,35 @@
-import React, { useState } from 'react'
-import {
-    Collapse,
-    NavbarToggler,
-  } from 'reactstrap';
+import React, { useState, useEffect } from 'react'
+import { Collapse, NavbarToggler } from 'reactstrap';
 import { Link } from 'react-router-dom'
 
-class HomeNav extends React.Component{
-    /* Creating a General Navegation Bar Component.
-        :parram - home: Boolean indicating if the Navegation Bar 
-                        is for Home Page or Logging Page
-    */
+function NavBar(){
 
-    constructor(){
-        super()
-        this.state = {
-            home: {
-                links: ['Questions', 'Contacts', 'Messages' , 'Logout'],
-                route: ['/questions', '#', '#', '/logout']
-            }
-        }
-    }
-
-    navBarLinks(){
-
-        const tagLinks = this.state.home.links.map(
-            (element, index) => <NavBarContent key={index} 
-                                                content={element} 
-                                                route={this.state.home.route[index]}/>
-            )
-        return tagLinks
-    }
-
-    render(){
-
-        const tagLinks = this.navBarLinks()
-
-        return(
-            <DefaultNavBar iconLink={'/home'}>
-                {tagLinks}
-            </DefaultNavBar>
-        )
-    }
-}
-
-
-function DefaultNavBar(props){
-    /* Creating the Basic Template for the Navegation Bar 
-        :parram - Children Component: Place reserved for adding links <li> 
-        :return - Navegation Bar template
-    */
-   
+    const [linkElements, setLinkElements] = useState(['Questions', 'Contacts', 'Messages' , 'Logout'])
+    const [linkRoute, setLinkRoute] = useState(['/questions', '#', '#', '/logout'])
+    const [links, setLinks] = useState(() => linkElements.map( (element, index) => <NavBarContent key={index} 
+                                                                                            content={element} 
+                                                                                            route={linkRoute[index]}/>))
     const [isOpen, setIsOpen] = useState(false);
-
     const toggle = () => setIsOpen(!isOpen);
-
-
 
     return(
         <div>
             <nav className="navbar navbar-expand-sm navbar-light bg-steel">
-                <Link className="navbar-brand text-white" to={props.iconLink}>Ask Me!</Link>
+                <Link className="navbar-brand text-white" to='/home'>Ask Me!</Link>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>   
                     <ul className="navbar-nav ml-auto">
-                        {props.children}
+                        {links}
                     </ul>
                 </Collapse>
             </nav>
         </div>
         
     )
+    
 }
 
-function NavBarContent(props){
-    /* Generating HTML Navegation Bar links
-        :parram: content - Name of the new Link
-        :return: Navegation Bar link
-    */
-
-
+const NavBarContent = (props) => {
     return(
         <li className="nav-item active">
             <Link className="nav-link text-white" to={props.route}>{props.content}<span className="sr-only">(current)</span></Link>
@@ -87,4 +38,4 @@ function NavBarContent(props){
 }
 
 
-export default HomeNav
+export default NavBar
--- src/app/components/homeComponents/userCard.js
+++ src/app/components/homeComponents/userCard.js
@@ -1,38 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-class UserCard extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            pic: ""
-        }
-    }
+function UserCard(){
 
-    componentDidMount(){
+    const [pic, setPic] = useState()
+
+    useEffect(() => {
         fetch("http://192.168.0.23:5000/profilepic",{
             method: "GET",
         })
         .then(res => res.blob())
-        .then(res => this.setState({pic: URL.createObjectURL(res)}) )
+        .then(res => setPic( URL.createObjectURL(res) ) )
+    }, [])
 
-    }
+    return(
+        <article className="media content-section">
+            <img className="rounded-circle article-img" src={pic} />
+            <div className="media-body">
+                <h4 className="article-metadata title text-center text-md-left">
+                    <a className="article-title" href="#">
+                        Hi {localStorage.getItem('UID')}
+                    </a>
+                </h4>
+                <p className="mt-3 text-center text-sm-left">9999 New Messages</p>
+                <p className="text-center text-sm-left">9999 New Invitations</p>
+            </div>
+        </article>
+    )
 
-    render(){
-        return(
-            <article className="media content-section">
-                <img className="rounded-circle article-img" src={this.state.pic} />
-                <div className="media-body">
-                    <h4 className="article-metadata title text-center text-md-left">
-                        <a className="article-title" href="#">
-                            Hi {localStorage.getItem('UID')}
-                        </a>
-                    </h4>
-                    <p className="mt-3 text-center text-sm-left">9999 New Messages</p>
-                    <p className="text-center text-sm-left">9999 New Invitations</p>
-                </div>
-            </article>
-        )
-    }
 
 }
 
