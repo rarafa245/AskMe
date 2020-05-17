fd9f2cfcd137e5a9591c6b51710dfd12047e7fae
Rafael <rarafa245@gmail.com>
2020-05-17 00:04:39 -0300

    Refactoring navBar Hook

--- src/app/components/logginComponents/navbar.js
+++ src/app/components/logginComponents/navbar.js
@@ -1,85 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Collapse,
     NavbarToggler,
   } from 'reactstrap';
 import { Link } from 'react-router-dom'
 
-class NavBar extends React.Component{
-    /* Creating a General Navegation Bar Component.
-        :parram - home: Boolean indicating if the Navegation Bar 
-                        is for Home Page or Logging Page
-    */
+function NavBar(){
 
-    constructor(){
-        super()
-        this.state = {
-            login: {
-                links: ['Create a New Account', 'Contact Me!', 'About'],
-                route: ['/register', '#', '#']
-            }
-        }
-    }
-
-    navBarLinks(){
-
-        const tagLinks = this.state.login.links.map(
-            (element, index) => <NavBarContent key={index} 
-                                                content={element} 
-                                                route={this.state.login.route[index]}/>
-            )
-        return tagLinks
-    }
-
-    render(){
-
-        const tagLinks = this.navBarLinks()
-
-
-        return(
-            <DefaultNavBar iconLink={'/'}>
-                {tagLinks}
-            </DefaultNavBar>
-        )
-    }
-}
-
-
-function DefaultNavBar(props){
-    /* Creating the Basic Template for the Navegation Bar 
-        :parram - Children Component: Place reserved for adding links <li> 
-        :return - Navegation Bar template
-    */
-   
+    const [linkElements, setLinkElements] = useState(['Create a New Account', 'Contact Me!', 'About'])
+    const [linkRoute, setLinkRoute] = useState(['/register', '#', '#'])
+    const [links, setLinks] = useState(() => linkElements.map( (element, index) => <NavBarContent key={index} 
+                                                                                            content={element} 
+                                                                                            route={linkRoute[index]}/>))
     const [isOpen, setIsOpen] = useState(false);
-
     const toggle = () => setIsOpen(!isOpen);
-
-
 
     return(
         <div>
             <nav className="navbar navbar-expand-sm navbar-light bg-steel">
-                <Link className="navbar-brand text-white" to={props.iconLink}>Ask Me!</Link>
+                <Link className="navbar-brand text-white" to='/'>Ask Me!</Link>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>   
                     <ul className="navbar-nav ml-auto">
-                        {props.children}
+                        {links}
                     </ul>
                 </Collapse>
             </nav>
         </div>
         
     )
+    
 }
 
+
 function NavBarContent(props){
-    /* Generating HTML Navegation Bar links
-        :parram: content - Name of the new Link
-        :return: Navegation Bar link
-    */
-
-
     return(
         <li className="nav-item active">
             <Link className="nav-link text-white" to={props.route}>{props.content}<span className="sr-only">(current)</span></Link>
